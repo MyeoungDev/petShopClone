68e619d7155a8d96c4ccf0c02fdaf2d644587487
KangMyeoungGwan <lung4536@naver.com>
2021-01-18 20:03:12 +0900

    header, swipe menu (Swiper 라이브러리 사용) , banner(Swiper 라이브러리 사용) commit

--- public/js/swipe.js
+++ public/js/swipe.js
@@ -1,6 +1,27 @@
-/*
-마우스 좌표 x,y 를 0으로 설정
-그리고 touch x,y가 움직이면
-다음 내용들을 display none에서 block으로 변경 시켜주고
-transition left right로 나타나게 표현해준다?
-*/
+var swiper = new Swiper('.swiper1', {
+    slidesPerView: 4,
+    spaceBetween: -20,
+    // centeredSlides: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+var swiper = new Swiper('.swiper2', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
