7fd0145e35aeb8395c37a95fac383350e870fa18
KangMyeoungGwan <lung4536@naver.com>
2021-01-19 17:56:03 +0900

    category 중간 commit

---
+++ public/js/category.js
@@ -0,0 +1,10 @@
+const category = document.querySelectorAll('.menu__category');
+
+// category.addEventListener('click', function(){
+    
+// })
+
+
+const categoryFunc = () => {
+    
+}
--- public/js/swipe.js
+++ public/js/swipe.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.swiper1', {
+var swiper1 = new Swiper('.swiper1', {
     slidesPerView: 4,
     spaceBetween: -20,
     // centeredSlides: true,
@@ -8,7 +8,7 @@ var swiper = new Swiper('.swiper1', {
     },
   });
 
-var swiper = new Swiper('.swiper2', {
+var swiper2 = new Swiper('.swiper2', {
     spaceBetween: 30,
     centeredSlides: true,
     loop: true,
@@ -25,3 +25,27 @@ var swiper = new Swiper('.swiper2', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+// var swiper = new Swiper ('.swiper3', {
+//     slidesPerView: 4,
+//     slidesPerColumn: 2,
+//     spaceBetween: 30,
+//     pagination: {
+//         el: '.swiper-pagination',
+//         type: 'bullets',
+//         clickable: true,
+//     },
+// });
+
+var swiper3 = new Swiper('.swiper3', {
+    slidesPerView: 3,
+    slidesPerColumn: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    // loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.menu__bar',
+      type: 'custom',
+      clickable: true,
+    },
+  });
